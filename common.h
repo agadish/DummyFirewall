7c5cbc462f3bfed846c870ecf224da2e25430925
Assaf Gadish <agadish@gmail.com>
2022-10-24 21:19:13 +0300

    Finished, before compiling

---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+/*   M A C R O S   */
+#defined UNUSED_ARG(a) ((void)(a))
+
+
+#endif /* __COMMON_H__ */
---
+++ hw1secws.c
@@ -0,0 +1,128 @@
+/*   I N C L U D E S   */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+#include "common.h"
+
+
+/*   F U N C T I O N S    D E C L A R A T I O N S   */
+static int __init hw1secws_init(void);
+
+static void __exit hw1secws_exit(void);
+
+static void log_accept(void);
+
+static void log_drop(void);
+
+static unsigned int hw1secws_hookfn_accept(
+    void *priv,
+    struct sk_buff *skb,
+    const struct nf_hook_state *state
+);
+
+static unsigned int hw1secws_hookfn_drop(
+    void *priv,
+    struct sk_buff *skb,
+    const struct nf_hook_state *state
+);
+
+
+/*   G L O B A L S   */
+static struct nf_hooks_op g_input_hook;
+static struct nf_hooks_op g_forward_hook;
+
+
+/*   F U N C T I O N S    I M P L E M E N T A T I O N S   */
+static void log_accept(void)
+{
+    printk(KERN_INFO "*** Packet accept ***\n");
+}
+
+static void log_drop(void)
+{
+    printk(KERN_INFO "*** Packet Dropped ***\n");
+}
+
+static bool_t is_source
+
+static unsigned int hw1secws_hookfn_accept(
+    void *priv,
+    struct sk_buff *skb,
+    const struct nf_hook_state *state
+){
+    UNUSED_ARG(priv);
+    UNUSED_ARG(skb);
+    UNUSED_ARG(state);
+
+    log_accept();
+
+    return NF_ACCEPT;
+}
+
+static unsigned int hw1secws_hookfn_drop(
+    void *priv,
+    struct sk_buff *skb,
+    const struct nf_hook_state *state
+){
+    UNUSED_ARG(priv);
+    UNUSED_ARG(skb);
+    UNUSED_ARG(state);
+
+    log_drop();
+
+    return NF_DROP;
+}
+
+static int __init hw1secws_init(void)
+{
+    int result = 0;
+    int result_register_hook = -1;
+
+    /* 1. Register input hook to accept all packets */
+    /* 1.1. Init struct fields */
+    g_input_hook.hook = hw1secws_hookfn_accept;
+    g_input_hook.hooknum = NF_IP_PRE_ROUTING;
+    g_input_hook.pf = PF_INET;
+    g_input_hook.priority = NF_IP_PRI_FIRST;
+
+    /* 1.2. Register hook */
+    result_register_hook = nf_register_hook(&g_input_hook);
+    if (0 != result_register_hook) {
+        result = result_register_hook;
+        goto l_cleanup;
+    }
+        
+    /* 2. Register forward hook to drop all packets */
+    /* 2.1. Init struct fields */
+    g_forward_hook.hook = hw1secws_hookfn_drop;
+    g_forward_hook.hooknum = NF_IP_PRE_ROUTING;
+    g_forward_hook.pf = PF_INET;
+    g_forward_hook.priority = NF_IP_PRI_FIRST;
+
+    /* 2.2. Register hook */
+    result_register_hook = nf_register_hook(&g_forward_hook);
+    if (0 != result_register_hook) {
+        result = result_register_hook;
+        goto l_cleanup;
+    }
+        
+    /* Success */
+    result = 0;
+l_cleanup:
+
+	return result;
+}
+
+static void __exit hw1secws_exit(void)
+{
+    nf_unregister_hook(&g_input_hook);
+    nf_unregister_hook(&g_forward_hook);
+}
+
+
+/*   K E R N E L   H O O K S   */
+module_init(hw1secws_init);
+module_exit(hw1secws_exit);
+
