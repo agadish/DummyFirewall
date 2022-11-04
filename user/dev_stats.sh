4ef43f5c7b319f52aeb75ee3622f642d7c48ca10
Assaf Gadish <agadish@gmail.com>
2022-11-04 12:23:33 +0200

    Fixed stats

--- user/dev_stats.sh
+++ user/dev_stats.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Configuration
-CLASS_NAME="hw2secws"
-DEVICE_NAME="hw2secws_class_hw2secws_class_char_device"
+CLASS_NAME="hw2secws_class"
+DEVICE_NAME="hw2secws_class_hw2secws_char_device"
 FILE_NAME="stats_accepted_dropped"
 FILE_PATH=/sys/class/$CLASS_NAME/$DEVICE_NAME/$FILE_NAME
 
 # 1. Read file
-IFS=, cat $FILE_PATH | read -r accepted_packets dropped_packets
+IFS="," read -r accepted_packets dropped_packets <<< $(cat $FILE_PATH)
 
 # 2. Print stats
 echo "Firewall Packets Summary:"
